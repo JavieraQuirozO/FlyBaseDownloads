942d020cc6c46016122e4364b4f811c43ee1fda8
Javiera.Quiroz <46948829+JavieraQuirozO@users.noreply.github.com>
2023-06-13 18:28:48 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 18:22:34 2023
+
+@author: usuario
+"""
+
+import FlyBaseDownloads as FBD
+
+#Ejemplo
+
+Sinonimos = FBD.Synonyms.get()
