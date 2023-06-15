9b3d916d7b359caed138b5b2d0750f633f25b16f
Javiera.Quiroz <46948829+JavieraQuirozO@users.noreply.github.com>
2023-06-14 21:06:08 -0300

    main.py

--- main.py
+++ main.py
@@ -21,3 +21,26 @@ Ontology = FBD.Ontology_Terms
 
 graph = Ontology.DO_ontology()
 
+#%%
+
+Genes = FBD.Genes
+
+NonRNA = Genes.Noncoding_RNAs()
+
+affy1 = Genes.FBgn_exons2affy1()
+
+#%%
+Organism = FBD.Organisms.Species_list()
+
+#%%
+Insertions = FBD.Insertions
+
+Gal4 = Insertions.GAL4_drivers()
+#%%
+map_in = Insertions.Map_insertions()
+
+#%%
+Clones = FBD.Clones.genomic_clone_data()
+
+#%%
+References = FBD.References.FBrf_PMid_PMCid_doi()
